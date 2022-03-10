ba5c550851d83d4840e97d4b00ab03844c45431d
dbrain <dbrain110@gmail.com>
2022-03-10 00:20:57 -0600

    - Added LiveDataTestUtil

--- app/src/test/java/com/shiftkey/codingchallenge/viewModel/ShiftViewModelTest.kt
+++ app/src/test/java/com/shiftkey/codingchallenge/viewModel/ShiftViewModelTest.kt
@@ -3,7 +3,11 @@ package com.shiftkey.codingchallenge.viewModel
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.shiftkey.codingchallenge.getOrAwaitValue
 import junit.framework.TestCase
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,31 +15,31 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ShiftViewModelTest : TestCase() {
 
+    private lateinit var shiftViewModel: ShiftViewModel
+
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    @Before
+    fun setupViewModel() {
+        shiftViewModel = ShiftViewModel(ApplicationProvider.getApplicationContext())
+    }
 
     @Test
     fun testGetErrorMessage() {
-        val shiftViewModel = ShiftViewModel(ApplicationProvider.getApplicationContext())
         shiftViewModel.errorMessage
     }
 
     @Test
     fun testGetLoading() {
-        val shiftViewModel = ShiftViewModel(ApplicationProvider.getApplicationContext())
         shiftViewModel.loading
+        assertThat(shiftViewModel.loading.getOrAwaitValue(), `is`(true))
     }
 
     @Test
     fun testShowShifts() {
-        val shiftViewModel = ShiftViewModel(ApplicationProvider.getApplicationContext())
-        shiftViewModel.showShifts()
+        val value = shiftViewModel.showShifts().getOrAwaitValue()
+        assertThat(value.toString(), not(nullValue()))
     }
 
-    @Test
-    fun testFetchShifts() {
-        val shiftViewModel = ShiftViewModel(ApplicationProvider.getApplicationContext())
-        shiftViewModel.fetchShifts()
-    }
 }
